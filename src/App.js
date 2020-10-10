44a918e0163b26090913526fea15539f689a5c7e
MPGA <monicagandino@gmail.com>
2020-10-10 21:40:46 +0200

    added popup to map and event listener

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import React,{ useState } from 'react';
-import ReactMapGL from "react-map-gl";
+import React,{ useState, useEffect } from 'react';
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
+import * as buildings from "./Data/only_buildings.json";
 
 
 
 
 export default function App() {
   const [viewport, setViewport] = useState({
-    latitude: 55.6867243,
-    longitude:12.5700724,
+    latitude: 55.690507,
+    longitude:12.587300,
     width: "100vw",
-    height: "100vh",
+    height: "100vw",
     zoom: 12
   });
+
+  const [selectedBuilding, setSelectedBuilding] = useState(null);
+
+  useEffect(()=> {
+    const listener = e => {
+      if (e.key==="Escape") {
+        setSelectedBuilding(null);
+      }
+    };
+    window.addEventListener("keydown",listener);
+    return() => {
+      window.removeEventListener("keydown",listener);
+    };
+  },[]);
+
 
   return ( 
   <div>
     <ReactMapGL 
     {...viewport} 
-    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}> 
+    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+    mapStyle="mapbox://styles/mpga/cjdrbn31f292v2ss3o55ssx2y"
+    onViewportChange={viewport => {
+      setViewport(viewport);
+    }}
+    > 
+    {buildings.features.map(building => (
+      <Marker 
+      key={building.properties.ID}
+      latitude={building.geometry.coordinates[1]}
+      longitude={building.geometry.coordinates[0]}
+      >  
+      < button
+        className="marker-btn"
+        onClick={e =>{
+          e.preventDefault();
+          setSelectedBuilding(building);
+        }}
+        >
+          <img src="/point.svg" alt="Blue dot"/>
+        </button>
+      </Marker>
+
+    ))}
+
+    {selectedBuilding ? (
+      <Popup
+      latitude={selectedBuilding.geometry.coordinates[1]}
+      longitude={selectedBuilding.geometry.coordinates[0]}
+      onClose={()=> {
+        setSelectedBuilding(null);
+    }}
+    >
+      <div>
+        <h1>{selectedBuilding.properties.anlaegsbet}</h1>
+        <h1>{selectedBuilding.properties.datering}</h1>
+      </div>
+      </Popup>
+    ) : null}
+
     </ReactMapGL>
   </div>
   );
