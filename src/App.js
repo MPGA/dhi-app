7de73246cb29215b1c109e54fd1faf5d693c89cf
MPGA <monicagandino@gmail.com>
2020-10-10 02:00:56 +0200

    create MapBoxGL function, added dotenv to package.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import ReactMapGL from "react-map-gl";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 55.6867243,
+    longitude:12.5700724,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10
+  });
+
+  return ( 
+  <div>
+    <ReactMapGL 
+    {...viewport} 
+    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}>
+    </ReactMapGL>
+  </div>
   );
 }
-
-export default App;
