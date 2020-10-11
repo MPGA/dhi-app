de63dcfb12b3148453fb507a76c9625ce257a28e
MPGA <monicagandino@gmail.com>
2020-10-12 01:21:40 +0200

    added List Icon

---
+++ src/components/List.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {List, ListItem, ListItemIcon, ListItemText, Divider} from '@material-ui/core';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import BubbleChartIcon from '@material-ui/icons/BubbleChart';
+
+
+
+
+const ListDrawer = () => {
+    return (
+        <div>
+            <List component='nav'>
+
+
+                <ListItem buttom>
+                    <ListItemIcon>
+                        <BubbleChartIcon/>
+                    <ListItemText primary='Test'/>
+                     </ListItemIcon>
+                     </ListItem>
+
+                <ListItem buttom>
+                    <ListItemIcon>
+                        <TimelineIcon/>
+                    <ListItemText primary='Test'/>
+                     </ListItemIcon>     
+                </ListItem>
+            <Divider/>
+            </List>
+        </div>
+    )
+}
+
+
+
+
+export default ListDrawer; 
