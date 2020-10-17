23d2bc5f2260eafb9e9342b520b9d2435c0a6044
Monica Gandino <monicagandino@Monicas-MacBook-Pro.local>
2020-10-17 11:25:09 +0200

    fixed function - added function trees

--- src/components/FixData.js
+++ src/components/FixData.js
@@ -1,10 +1,24 @@
+import * as tree from "../Data/cph_trees.json";
+
 export default function prepareData(dataMap, option){
     const array = dataMap.dataMap.properties.timeseries;
     const today = dataMap.dataMap.properties.meta.updated_at.slice(0,10); 
+    var tomorrow;
     var i;
+    var date = new Date();
+    var count = 0;
     var result = [];
     for (i = 0; i < array.length; i++){
-        if(array[i].time.includes("T00")){
+        if(array[i].time.includes(date.getDate()+"T")){
+            count = count + 1;
+        }
+    }
+    if(count < 4)
+    {
+        tomorrow = String(date.getDate() + 1) + "T";
+    }
+    for (i = 0; i < array.length; i++){
+        if(array[i].time.includes(today) || array[i].time.includes(tomorrow)){
             if(option == 1){
                 var obj = {"Temperature C°": array[i].data.instant.details.air_temperature, "time": "Hour: "+array[i].time.slice(11,13)};
                 result.push(obj)
@@ -16,4 +30,23 @@ export default function prepareData(dataMap, option){
         }
     }
     return result;
+}
+
+export function prepareDataTree(){
+    var i;
+    var data = [];
+    for (i = 0; i < tree.features.length; i++){
+        if(data.filter(obj => obj.Year === tree.features[i].properties.planteaar).length > 0){
+            var index = data.findIndex(o=> o.Year === tree.features[i].properties.planteaar)
+            data[index].Planted = data[index].Planted + 1;
+        }
+        else {
+            var year = tree.features[i].properties.planteaar
+            var obj = {"Year": year, "Planted":1}
+            data.push(obj);   
+
+        }
+    }
+
+    return data;
 }
